c341bbc3228e734baf586e0c8696594953d622aa
Aboyislive <ebubechukwuekeugo@gmail.com>
2026-04-09 17:18:49 +0100

    stable ll model fix

--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -1,14 +1,13 @@
 from google import genai
 from app.core.config import settings
 
-# Create a client instance with the API key
-client = genai.Client(api_key=settings.GEMINI_API_KEY)
 
 async def generate_response(message: str) -> str:
     try:
+        client = genai.Client(api_key=settings.GEMINI_API_KEY)
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
-            contents=message
+            model="gemma-3-1b-it",
+            contents=message,
         )
         return response.text
     except Exception as e:
