3b139a8d2f394c5ef0f12479ab21290c98fe139a
Aboyislive <ebubechukwuekeugo@gmail.com>
2026-04-09 10:42:34 +0100

    llm moodel change

--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -7,7 +7,7 @@ client = genai.Client(api_key=settings.GEMINI_API_KEY)
 async def generate_response(message: str) -> str:
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-2.0-flash",
             contents=message
         )
         return response.text
