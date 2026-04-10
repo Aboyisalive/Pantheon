70de50ebe0e718cd704325b9ef355b3b243b4937
Aboyislive <ebubechukwuekeugo@gmail.com>
2026-04-10 11:55:17 +0100

    db secrets

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
+
+load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
--- backend/app/schemas/chat.py
+++ backend/app/schemas/chat.py
@@ -7,13 +7,13 @@ class ChatCreate(BaseModel):
 
 class ChatOut(BaseModel):
     model_config = ConfigDict(from_attributes=True)
- 
+
     id: int
+    session_id: int
+    user_id: int
     message: str
     response: str
     created_at: datetime
-
-
 
 class SessionCreate(BaseModel):
     title: str | None = "New Chat"
