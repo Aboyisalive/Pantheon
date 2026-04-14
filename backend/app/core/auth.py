2c1ba7c80356b001ad630fa0b5e83ecc243ca7e7
Aboyislive <ebubechukwuekeugo@gmail.com>
2026-04-14 11:13:51 +0100

    password hash limit

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -14,13 +14,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 # Password hashing
-def hash_password(password: str):
-    password = password[:72]  # enforce bcrypt limit
-    return pwd_context.hash(password)
+def hash_password(password):
+    return pwd_context.hash(password[:72])
 
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
-
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 # JWT creation
 def create_access_token(data: dict):
